0bf4f2a57dbd0454ceef95f78e99c6c6bd24fec9
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-05-07 02:39:27 +0530

    server gets connected to client, gets user data from utils and emits event to rooms

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path')
 const http = require('http')
 const express = require("express")
 const { Server } = require('socket.io')
+const formatMessage = require('./utils/messages')
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users')
 
 const app = express()
 const httpServer = http.createServer(app)
@@ -16,22 +18,50 @@ app.use(express.static(path.resolve(__dirname, 'public')))
 
 ioServer.on('connection', socket => {
 
-    // emitting a new welcome message upon connection
-    socket.emit('message', 'Welcome to Chat App')
+    // 1. client connects -----------------
+    socket.on('joinRoom', ({ username, room }) => {
 
-    // broadcast joining message when there is new connection
-    socket.broadcast.emit('message', 'A user has joined the chat')
+        const user = userJoin(socket.id, username, room)
+        socket.join(user.room)
 
-    // broadcast leaving message when a user disconnects
-    socket.on('disconnect', () => {
-        ioServer.emit('message', 'A user has left the chat')
+        // emitting a new welcome message upon connection
+        socket.emit('message', formatMessage('Bot', 'Welcome to Chat App'))
+
+        // broadcast to room joining message when there is new connection
+        socket.to(user.room).emit('message', formatMessage('Bot', `${user.username} has joined the chat`))
+
+        // send users and room info
+        ioServer.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        })
     })
 
-    // listening for chat event
+    // 2. listening for chat event ------------------
     socket.on('chat', (message) => {
 
+        const user = getCurrentUser(socket.id)
+
         // broadcast chat message
-        ioServer.emit('message', message)
+        ioServer.to(user.room).emit('message', formatMessage(user.username, message))
+    })
+
+    // 3. broadcast leaving message when a user disconnects ------------------
+    socket.on('disconnect', () => {
+
+        const user = userLeave(socket.id)
+
+        if (user) {
+
+            ioServer.to(user.room).emit('message', formatMessage('Bot', `${user.username} user has left the chat`))
+
+            // send users and room info
+            ioServer.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            })
+        }
+
     })
 })
 
